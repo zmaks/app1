28c7707cc713e7214f552d705bc5c0bf0a0c07c1
zmaks <maks.zheltouhov@yandex.ru>
2017-11-01 11:20:39 +0300

    mesage

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello 123");
         }
     }
 }
